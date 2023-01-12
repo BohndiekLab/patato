ea032d37b449613d2f8a3bdabaf0abf654a2f0a1
Thomas Else <56722586+tomelse@users.noreply.github.com>
2023-01-12 16:58:25 +0000

    Update examples

--- patato/data/__init__.py
+++ patato/data/__init__.py
@@ -8,5 +8,5 @@ PATATO Data
 PATATO Data - an interface for exemplar data and basic simulations.
 """
 
-from simulated_datasets import get_basic_p0, generate_basic_simulation
-from get_example_datasets import get_msot_time_series_example
+from .simulated_datasets import get_basic_p0, generate_basic_simulation
+from .get_example_datasets import get_msot_time_series_example
