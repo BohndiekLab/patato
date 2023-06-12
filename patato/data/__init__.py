5102c65ae18e5036e146954ccaaee6be33bf4202
Thomas Else <56722586+tomelse@users.noreply.github.com>
2023-06-12 12:37:40 +0100

    Change structure of data module

--- patato/data/__init__.py
+++ patato/data/__init__.py
@@ -11,3 +11,11 @@ PATATO data - an interface for exemplar data and basic simulations to illustrate
 from .simulated_datasets import get_basic_p0, generate_basic_simulation
 from .get_example_datasets import get_msot_time_series_example, get_ithera_msot_time_series_example, \
     get_msot_phantom_example
+
+__all_exports = [get_msot_time_series_example, get_ithera_msot_time_series_example,
+                 get_msot_phantom_example, get_basic_p0, generate_basic_simulation]
+
+for e in __all_exports:
+    e.__module__ = __name__
+
+__all__ = [e.__name__ for e in __all_exports]
