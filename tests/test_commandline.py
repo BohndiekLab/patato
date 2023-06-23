ecd312b16a624c93f3df5b40a6494b063e207dde
Thomas Else <56722586+tomelse@users.noreply.github.com>
2023-06-23 11:54:51 +0100

    Update convenience script tests

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -16,7 +16,9 @@ class TestCommandLine(unittest.TestCase):
 
     @mock.patch('argparse.ArgumentParser.parse_args',
                 return_value=argparse.Namespace(input="test_data_ithera/Scan_9",
-                                                output="test_pipeline_folder"))
+                                                output="test_pipeline_folder",
+                                                update=False,
+                                                name=False))
     def test_import_ithera(self, mock_args):
         from patato.convenience_scripts.convert_binary_to_hdf5 import main
         main()
@@ -24,22 +26,44 @@ class TestCommandLine(unittest.TestCase):
     @mock.patch('argparse.ArgumentParser.parse_args',
                 return_value=argparse.Namespace(input=".",
                                                 c=1500,
+                                                preset=None,
+                                                run=None,
                                                 clear=True,
-                                                console=True))
+                                                console=True,
+                                                scan=None,
+                                                startscan=None,
+                                                wavelength=None,
+                                                usthreshold=None,
+                                                log=False,
+                                                lineplots=False))
     def test_tune_speed_of_sound(self, mock_args):
         from patato.convenience_scripts.tune_speed_of_sound import main
         main()
 
     @mock.patch('argparse.ArgumentParser.parse_args',
                 return_value=argparse.Namespace(input=".",
-                                                clear=True,))
+                                                speed=None,
+                                                output=None,
+                                                preset=None,
+                                                run=None,
+                                                wavelength=None,
+                                                repeat=True,
+                                                highpass=None,
+                                                lowpass=None,
+                                                clear=False,
+                                                ipasc=False,
+                                                debug=False))
     def test_process_msot(self, mock_args):
         from patato.convenience_scripts.process_msot import main
         main()
 
     @mock.patch('argparse.ArgumentParser.parse_args',
                 return_value=argparse.Namespace(input=".",
-                                                clear=True, ))
+                                                wavelength=[],
+                                                wavelengthindices=[],
+                                                preset=None,
+                                                clear=True,
+                                                filter=None))
     def test_unmix(self, mock_args):
         from patato.convenience_scripts.unmix import main
         main()
